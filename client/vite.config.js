bbc42f1b9906b8213ae9e946d681950e7de7202a
atul <atul22gcoders@gmail.com>
2024-01-23 13:42:17 +0530

    improve 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/MernDashboard/",
-  base: "/https://atugatran-projects.github.io/MernDashboard/",
+  base: "/MernDashboard/",
   plugins: [react()],
 })
