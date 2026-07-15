ed968047f822b1d28046f8acdc3821717327769e
Atul <atul22gcoders+atugatran@gmail.com>
2026-07-16 04:22:42 +0530

    change link

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://merndashboard-n4db.onrender.com/",
+  base: "https://mern-dashboard-atul22g-dev.vercel.app/",
   plugins: [react()],
 })
